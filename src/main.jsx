ef196886ed4944a9fc4f61ea3935b36b1fbba942
Docsant <vampiroazul1315@gmail.com>
2026-06-18 23:24:08 -0400

    fix directory path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import { AppProvider, useApp } from './Context/AppContext.jsx';
+import { AppProvider, useApp } from './context/AppContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
